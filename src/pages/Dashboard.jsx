ace4b21b49b92dc5ef2f571113e2dd18ca21678c
Semon Benyamen <semobenyamen@gmail.com>
2026-04-28 17:17:40 +0300

    first Phase

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Dashboard/Footer/Footer.jsx";
+import Navbar from "../components/Dashboard/Navbr/Navbar.jsx";
+
+function Dashboard() {
+    return(
+        <>
+        <Navbar adminName="Semon"/>
+        <h1>Test Dashboard</h1>
+        <Footer />
+        </>
+    );
+}
+
+export default Dashboard;
